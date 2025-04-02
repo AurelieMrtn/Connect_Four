e983fc988aac6a141badc4349ab9a7955d2f016b
Aurelie MARTIN <aurelie.martin95@hotmail.fr>
2025-04-02 11:24:08 +0200

    feat(board): victory conditions added

--- src/Board.java
+++ src/Board.java
@@ -38,5 +38,43 @@ class Board {
     }
 
     // Victory conditions (horizontal, vertical, diagonal)
+    protected boolean checkWin(char symbol) {
+        return checkHorizontal(symbol) || checkVertical(symbol) || checkDiagonal(symbol);
+    }
 
+    private boolean checkHorizontal(char symbol) {
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS - 3; j++) {
+                if (grid[i][j] == symbol && grid[i][j + 1] == symbol && grid[i][j + 2] == symbol && grid[i][j + 3] == symbol) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkVertical(char symbol) {
+        for (int i = 0; i < ROWS - 3; i++) {
+            for (int j = 0; j < COLUMNS; j++) {
+                if (grid[i][j] == symbol && grid[i + 1][j] == symbol && grid[i + 2][j] == symbol && grid[i + 3][j] == symbol) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkDiagonal(char symbol) {
+        // Bottom-left to top-right
+        for (int i = 3; i < ROWS; i++) {
+            for (int j = 0; j < COLUMNS - 3; j++) {
+                if (grid[i][j] == symbol && grid[i - 1][j + 1] == symbol && grid[i - 2][j + 2] == symbol && grid[i - 3][j + 3] == symbol) return true;
+            }
+        }
+
+        // Top-left to bottom-right
+        for (int i = 0; i < ROWS - 3; i++) {
+            for (int j = 0; j < COLUMNS - 3; j++) {
+                if (grid[i][j] == symbol && grid[i + 1][j + 1] == symbol && grid[i + 2][j + 2] == symbol && grid[i + 3][j + 3] == symbol) return true;
+            }
+        }
+
+        return false;
+    }
 }
