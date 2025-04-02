3c231c3dc2bf31e0409efd8e3c6451f68b3708ad
Aurelie MARTIN <aurelie.martin95@hotmail.fr>
2025-04-02 11:32:42 +0200

    feat(board): win check verification

--- src/Connect_Four.java
+++ src/Connect_Four.java
@@ -5,7 +5,12 @@ public class Connect_Four {
         board.drop(4, 'O');
         board.drop(2, 'X');
         board.drop(4, 'O');
+        board.drop(4, 'O');
+        board.drop(4, 'O');
 
         board.displayBoard();
+        System.out.println(String.valueOf(board.checkWin('O')));
+        System.out.printf(String.valueOf(board.checkWin('X')));
+
     }
 }
